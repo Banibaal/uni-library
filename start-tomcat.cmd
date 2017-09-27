8e18d1e1d1dcd37a01303901a995f044492227e4
Banibal <anael.b@gmail.com>
2017-09-27 10:25:35 +0100

    Enh : tomcat management

--- start-tomcat.cmd
+++ start-tomcat.cmd
@@ -2,16 +2,5 @@ echo "-----------------------"
 echo "Start tomcat instance"
 echo "-----------------------"
 
-SET CATALINA_HOME="C:\formation\servers\apache-tomcat-8.5.20"
+net start tomcat8
 
-echo "Using tomcat %CATALINA_HOME%"
-
-%CATALINA_HOME%\bin\catalina.bat start
-
-echo "----------------------"
-echo "tomcat is starting    "
-
-timeout 5
-
-echo "tomcat started       "
-echo "---------------------"
