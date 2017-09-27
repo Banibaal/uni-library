2b7a60c21a0baf73498bf09df05b8934d33e8cf4
Banibal <anael.b@gmail.com>
2017-09-27 10:47:47 +0100

    fix

--- start-tomcat.cmd
+++ start-tomcat.cmd
@@ -2,5 +2,17 @@ echo "-----------------------"
 echo "Start tomcat instance"
 echo "-----------------------"
 
-net start tomcat8
+REM SET CATALINA_HOME="C:\Program Files\Apache Software Foundation\Tomcat 8.0"
 
+REM echo "Using tomcat %CATALINA_HOME%"
+
+REM %CATALINA_HOME%\bin\tomcat.exe start
+
+echo "----------------------"
+echo "tomcat is starting    "
+
+net start Tomcat8
+timeout 2
+
+echo "tomcat started       "
+echo "---------------------"
